b8bf385205489819a9ad16745ae73cf58b9a82d1
mjkvasconcelos-eng <mjk.vasconcelos@gmail.com>
2026-09-06 00:47:07 -0300

    Atualizar cache do PWA e incluir ícones de instalação

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ora-es-v4';
-const FILES=['./','./index.html','./style.css','./app.js','./colecao2.js','./voice.js','./install.js','./manifest.json','./biblia.html','./biblia.css','./biblia.js'];
+const CACHE='ora-es-v5';
+const FILES=['./','./index.html','./style.css','./app.js','./colecao2.js','./voice.js','./install.js','./manifest.json','./icon-192.png','./icon-512.png','./biblia.html','./biblia.css','./biblia.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))))});
